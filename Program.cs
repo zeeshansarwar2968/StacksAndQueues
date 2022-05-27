59552077b0292ae2933be0efcc41a8d2963d0539
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-27 14:21:07 +0530

    [Zeeshan] : Added logic for enqueue operations

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ namespace StacksAndQueues
             Console.WriteLine("1.Push operation");
             Console.WriteLine("2.Pop and Peek operation");
             Console.WriteLine("------------------------------------------");
+            Console.WriteLine("3.Queue Operations");
+            Console.WriteLine("------------------------------------------");
             Console.Write("Enter the option : ");
             int num = Convert.ToInt32(Console.ReadLine());
             Stack stack = new Stack();
@@ -30,6 +32,30 @@ namespace StacksAndQueues
                     stack.Push(56);
                     stack.Pop();
                     stack.Display();
+                    break;
+                case 3:
+                    string flag1 = "Y";
+                    while (flag1 == "Y" || flag1 == "y")
+                    {
+                        Console.WriteLine("***1.Enqueue Operation***");
+                        Console.Write("Enter the option : ");
+                        int num1 = Convert.ToInt32(Console.ReadLine());
+                        Queue queue = new Queue();
+                        switch (num1)
+                        {
+                            case 1:
+                                queue.Enqueue(56);
+                                queue.Enqueue(30);
+                                queue.Enqueue(70);
+                                queue.Display();
+                                break;
+                            default:
+                                Console.WriteLine("Enter a valid option");
+                                break;
+                        }
+                        Console.WriteLine("\nDo you want to continue?(Y/N)");
+                        flag1 = Console.ReadLine();
+                    }
                     break;
                 default:
                     Console.WriteLine("Enter a valid option");
---
+++ Queue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class Queue
+    {
+        Node head;
+        
+        //Method/function to add node/data in queue
+        public void Enqueue(int item)
+        {
+            Node new_node = new Node(item);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+            }
+            Console.WriteLine("Inserted into queue : " + new_node.data);
+        }
+
+        //Method to display the nodes in the queue
+        public void Display()
+        {
+            Node temp = this.head;
+            if (this.head == null)
+            {
+                Console.WriteLine("No element present in the Queue");
+                return;
+            }
+            Console.WriteLine("The nodes are : ");
+            while (temp != null)
+            {
+
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+    }
+}
